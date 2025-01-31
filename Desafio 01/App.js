f31bcb06aaea1035fc1746e5d349a565bc539a21
CsPaulo <cspaulorafael@gmail.com>
2025-01-31 14:10:01 -0300

    trocando o alert pelo console.log

--- Desafio 01/App.js
+++ Desafio 01/App.js
@@ -1,66 +1,66 @@
 //1
 let nome = "Paulo Rafael";
-alert(nome);
+console.log(nome);
 
 //2
 let idade = 25,
   altura = 1.75;
-alert(idade + " anos e " + altura + " de altura");
+console.log(idade + " anos e " + altura + " de altura");
 
 //3
 let valor = 50,
   desconto = 0.2;
 let ValorDesconto = valor - valor * desconto;
-alert(`O valor com desconto é de R$ ${ValorDesconto}`);
+console.log(`O valor com desconto é de R$ ${ValorDesconto}`);
 
 //4
 let temperatura = 30;
 if (temperatura > 30) {
-  alert("Está calor!");
+  console.log("Está calor!");
 } else {
-  alert("Está frio!");
+  console.log("Está frio!");
 }
 
 //5
 let idade2 = 18;
 if (idade2 >= 18) {
-  alert("Maior de idade");
+  console.log("Maior de idade");
 } else {
-  alert("Menor de idade");
+  console.log("Menor de idade");
 }
 
 //6
 let nota = 6;
 if (nota >= 7) {
-  alert("Aprovado");
+  console.log("Aprovado");
 } else if (nota >= 5 && nota < 7) {
-  alert("Recuperação");
+  console.log("Recuperação");
 } else {
-  alert("Reprovado");
+  console.log("Reprovado");
 }
 
 //7
 let numero1 = 10,
   numero2 = 20;
 if (numero1 == numero2) {
-  alert("Os números são iguais");
+  console.log("Os números são iguais");
 } else {
-  alert("Os números são diferentes");
+  console.log("Os números são diferentes");
 }
 
 //8
 let nome2 = "Paulo",
   idade3 = 20;
-alert(`Olá, meu nome é ${nome2} e eu tenho ${idade3} anos`);
+console.log(`Olá, meu nome é ${nome2} e eu tenho ${idade3} anos`);
 
 //9
 for (let i = 1; i <= 10; i++) {
-  alert(i);
+  console.log(i);
 }
 
 //10
-for (let numero3; numero3 != 5; numero3++) {
-  let = prompt("Digite um número");
+for (let numero3; numero3 !== 5; ) {
+  numero3 = parseInt(prompt("Digite um número"), 10);
 }
 
 //11
@@ -77,8 +77,8 @@ for (let i = 0; i <= 20; i++) {
 
 //13
 let raio = 5;
-Math.PI * Math.pow(raio, 2);
-alert(`A área do círculo de raio ${raio} é ${area}`); //ou consolelog, tanto faz
+let area = Math.PI * Math.pow(raio, 2);
+console.log(`A área do círculo de raio ${raio} é ${area}`);
 
 //14
 //Declara duas variáveis, numero4 e numero5, e atribui os valores 10 e 20 a elas
@@ -89,7 +89,7 @@ let numero4 = 10,
 let soma = numero4 + numero5;
 
 //Mostra um alerta com a mensagem "A soma de 10 e 20 é 30"
-alert(`A soma de ${numero4} e ${numero5} é ${soma}`);
+console.log(`A soma de ${numero4} e ${numero5} é ${soma}`);
 
 //15
 function calcularSoma(x, y) {
@@ -103,4 +103,3 @@ let x = 10;
 let y = 20;
 let z = calcularSoma(x, y);
 exibirResultado(z);
-//caprichando nesse ultimo, os mais legivel possivel
